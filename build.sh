0c2b6bb9dfdb99006d9b34877cde3566abd85447
julianu <julian.uszkoreit@rub.de>
2024-05-21 09:43:45 +0200

    source container settings

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ cd docker
 
 for d in */; do
   cd $d
-  ./container.sh
+  . container.sh
   dir=$(echo ${d} | rev | cut -c 2- | rev)
   container_tag="quay.io/medbioinf/${dir}:${CONTAINER_VERSION}"
-  echo "$(env)"
   echo "building ${container_tag}"
+  
   docker build -t ${container_tag} -f Dockerfile .
 
   docker push ${container_tag}
