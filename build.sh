3fb8099c3711fdddf434fca031bfc3aa491b76a6
julianu <julian.uszkoreit@rub.de>
2024-05-21 09:38:56 +0200

    debug output

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ for d in */; do
   cd $d
   ./container.sh
   dir=$(echo ${d} | rev | cut -c 2- | rev)
-  container_tag=quay.io/medbioinf/${dir}:${CONTAINER_VERSION}
+  container_tag="quay.io/medbioinf/${dir}:${CONTAINER_VERSION}"
+  echo "$(env)"
   echo "building ${container_tag}"
   docker build -t ${container_tag} -f Dockerfile .
 
