154d85452d4acddb8508b1cfb91eb8d4288ab2c0
julianu <julian.uszkoreit@rub.de>
2025-08-26 18:49:17 +0200

    better base images and more stable dependencies

--- docker/mzid-merger/container.sh
+++ docker/mzid-merger/container.sh
@@ -2,4 +2,5 @@
 
 export CONTAINER_VERSION=1.4.26
 
-export MZIDMERGER_VERSION=v1.4.26
+DOWNLOAD_URL="https://github.com/PNNL-Comp-Mass-Spec/MzidMerger/releases/download/v1.4.26/MzidMerger.zip"
+export CONTAINER_BUILD_ARGS="--build-arg DOWNLOAD_URL=${DOWNLOAD_URL}"
--- docker/sage/container.sh
+++ docker/sage/container.sh
@@ -2,4 +2,7 @@
 
 export CONTAINER_VERSION=v0.15.0-beta.1
 
-export SAGE_VERSION=$CONTAINER_VERSION
+SAGE_VERSION=$CONTAINER_VERSION
+
+DOWNLOAD_URL="https://github.com/lazear/sage/releases/download/${SAGE_VERSION}/sage-${SAGE_VERSION}-x86_64-unknown-linux-musl.tar.gz"
+export CONTAINER_BUILD_ARGS="--build-arg SAGE_VERSION=${SAGE_VERSION} --build-arg DOWNLOAD_URL=${DOWNLOAD_URL}"
