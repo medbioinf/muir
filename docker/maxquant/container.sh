72041ab5a26838fdb40e20f20f8f4d1a42472e9c
julianu <julian.uszkoreit@rub.de>
2025-08-29 09:32:43 +0200

    build version 2.6.1.0

---
+++ docker/maxquant/container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export CONTAINER_VERSION=2.6.1.0
+
+DOWNLOAD_URL="https://gitlab.ruhr-uni-bochum.de/uszkojy2/maxquant-distributables/-/raw/main/v${CONTAINER_VERSION}/MaxQuant_v${CONTAINER_VERSION}.zip"
+export CONTAINER_BUILD_ARGS="--build-arg DOWNLOAD_URL=${DOWNLOAD_URL}"
