19d5a68a408aee3946d3f9667e20e3cb82c94263
julianu <julian.uszkoreit@rub.de>
2026-08-28 09:06:40 +0200

    remove comet (now in biocontainers)

--- docker/comet-ms/container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CONTAINER_VERSION=v2026.01.1
-
-DOWNLOAD_URL=https://github.com/UWPR/Comet/releases/download/${CONTAINER_VERSION}/comet.linux.exe
-
-export CONTAINER_BUILD_ARGS="--build-arg DOWNLOAD_URL=${DOWNLOAD_URL}"
